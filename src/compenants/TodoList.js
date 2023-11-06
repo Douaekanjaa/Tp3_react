78c3d189087e630c145aaf43e7d8a09947bde24e
Douaekanjaa <kanjaadouae1@gmail.com>
2023-11-06 13:13:29 +0100

    up

--- src/compenants/TodoList.js
+++ src/compenants/TodoList.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './TodoList.css';
+import trash from '../images/trash.png';
+import edit from '../images/edit.png';
 
 function TodoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
+  const [editTask, setEditTask] = useState(null);
+  const [editedTaskText, setEditedTaskText] = useState('');
 
   const handleTaskChange = (event) => {
     setNewTask(event.target.value);
@@ -11,36 +16,71 @@ function TodoList() {
 
   const addTask = () => {
     if (newTask !== '') {
-      setTasks((prevTasks) => [...prevTasks, newTask]);
+      setTasks((prevTasks) => [
+        ...prevTasks,
+        { id: uuidv4(), text: newTask, completed: false }
+      ]);
       setNewTask('');
     }
   };
 
-  const removeTask = (index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks.splice(index, 1);
+  const startEditing = (id, text) => {
+    setEditTask(id);
+    setEditedTaskText(text);
+  };
+
+  const saveEditedTask = (id) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, text: editedTaskText } : task
+    );
+    setTasks(updatedTasks);
+    setEditTask(null);
+  };
+
+  const removeTask = (id) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
     setTasks(updatedTasks);
   };
 
   return (
-    <div className="TodoList">
-      <h1>To-Do List</h1>
-      <div className="task-input">
-        <input
-          type="text"
-          placeholder="Add a new task"
-          value={newTask}
-          onChange={handleTaskChange}
-        />
-        <button onClick={addTask}>Add</button>
+    <div id='frag'>
+      <div  className='TodoList2'>
+          <h1 id='pink'>To-Do List</h1>
+        </div>
+      <div className="TodoList">
+        
+      
+        <div className="task-input ">
+          <input
+            type="text"
+            placeholder="Add a new task"
+            value={newTask}
+            onChange={handleTaskChange}
+          />
+          <button onClick={addTask}>Add</button>
+        </div>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              {editTask === task.id ? (
+                <div>
+                  <input
+                    type="text"
+                    value={editedTaskText}
+                    onChange={(e) => setEditedTaskText(e.target.value)}
+                  />
+                  <button onClick={() => saveEditedTask(task.id)}>Save</button>
+                </div>
+              ) : (
+                <span>{task.text}</span>
+              )}
+              <img src={edit} className='pics' onClick={() => startEditing(task.id, task.text)} alt='edit' style={{marginLeft: "30%", marginRight: "10px"}}/>
+              <img src={trash}  className='pics' alt='remove' onClick={() => removeTask(task.id)}  />
+                <br />
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task} <button onClick={() => removeTask(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
