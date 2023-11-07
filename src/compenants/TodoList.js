d92ab0c5c9ba569bb48fa14fc8a3e5f4837c9012
Douaekanjaa <kanjaadouae1@gmail.com>
2023-11-07 15:18:03 +0100

    updating the edit task methode

--- src/compenants/TodoList.js
+++ src/compenants/TodoList.js
@@ -3,13 +3,14 @@ import { v4 as uuidv4 } from 'uuid';
 import './TodoList.css';
 import trash from '../images/trash.png';
 import edit from '../images/edit.png';
+import check from '../images/check.png'
 
 function TodoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
   const [editTask, setEditTask] = useState(null);
   const [editedTaskText, setEditedTaskText] = useState('');
-
+  const [isEditing, setIsEditing] = useState(false);
   const handleTaskChange = (event) => {
     setNewTask(event.target.value);
   };
@@ -27,14 +28,20 @@ function TodoList() {
   const startEditing = (id, text) => {
     setEditTask(id);
     setEditedTaskText(text);
+    setIsEditing(true);
   };
 
-  const saveEditedTask = (id) => {
-    const updatedTasks = tasks.map((task) =>
-      task.id === id ? { ...task, text: editedTaskText } : task
-    );
-    setTasks(updatedTasks);
-    setEditTask(null);
+  const saveEditedTask = (id, text) => {
+    if (text === "") {
+      alert("You can't add an empty task");
+    } else {
+      const updatedTasks = tasks.map((task) =>
+        task.id === id ? { ...task, text } : task
+      );
+      setTasks(updatedTasks);
+      setEditTask(null);
+      setIsEditing(false);
+    }
   };
 
   const removeTask = (id) => {
@@ -44,19 +51,12 @@ function TodoList() {
 
   return (
     <div id='frag'>
-      <div  className='TodoList2'>
-          <h1 id='pink'>To-Do List</h1>
-        </div>
+      <div className='TodoList2'>
+        <h1 id='pink'>To-Do List</h1>
+      </div>
       <div className="TodoList">
-        
-      
         <div className="task-input ">
-          <input
-            type="text"
-            placeholder="Add a new task"
-            value={newTask}
-            onChange={handleTaskChange}
-          />
+          <input type="text" placeholder="Add a new task" value={newTask} onChange={handleTaskChange} />
           <button onClick={addTask}>Add</button>
         </div>
         <ul>
@@ -64,19 +64,22 @@ function TodoList() {
             <li key={task.id}>
               {editTask === task.id ? (
                 <div>
-                  <input
-                    type="text"
-                    value={editedTaskText}
-                    onChange={(e) => setEditedTaskText(e.target.value)}
+                  <input type="text" value={editedTaskText}
+                    onChange={(e) => setEditedTaskText(e.target.value)} style={{outline: "none"}}
                   />
-                  <button onClick={() => saveEditedTask(task.id)}>Save</button>
+                  <img src={check} id='save' onClick={() => saveEditedTask(task.id, editedTaskText)} />
                 </div>
               ) : (
-                <span>{task.text}</span>
+                <span id='txt'>{task.text}</span>
               )}
-              <img src={edit} className='pics' onClick={() => startEditing(task.id, task.text)} alt='edit' style={{marginLeft: "30%", marginRight: "10px"}}/>
-              <img src={trash}  className='pics' alt='remove' onClick={() => removeTask(task.id)}  />
-                <br />
+              {isEditing && editTask === task.id ? null : (
+                <img src={edit} className='ed pics' onClick={() => startEditing(task.id, task.text)} alt='edit' style={{ marginLeft: "30%", marginRight: "10px" }} />
+              )}
+              {isEditing && editTask === task.id ? null : (
+                <img src={trash} className='tr pics' alt='remove' onClick={() => removeTask(task.id)} />
+              )}
+              <br />
+              <hr />
             </li>
           ))}
         </ul>
